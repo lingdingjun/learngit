cfaf5bb41a1ae28fc1db7add0cdaacaa0dd3b29f
凌鼎钧 <lingdingjun@foxmail.com>
2017-01-31 21:43:39 +0800

    2 dif ways to cal lirun

---
+++ lirun2.py
@@ -0,0 +1,10 @@
+i = int(raw_input('净利润（万元）:'))
+arr = [100,60,40,20,10,0]
+rat = [0.01,0.015,0.03,0.05,0.075,0.1]
+r = 0
+for idx in range(0,6):
+    if i>arr[idx]:
+        r+=(i-arr[idx])*rat[idx]
+        print (i-arr[idx])*rat[idx]
+        i=arr[idx]
+print r
